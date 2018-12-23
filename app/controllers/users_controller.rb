a8df296e83f2acc6b078f05d997a520a5eab7609
Galina Podstrechnaya <galinap684@gmail.com>
2018-12-23 18:31:31 -0500

    created the first step of the form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+  if @user.save
+    session[:user_id] = @user.id
+    redirect_to root_url, notice: "Thank you for signing up!"
+    #'/after_signup/:id(.:format)'
+  else
+    render '/new'
+  end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
+end
